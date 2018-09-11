4695dcfd8f57b189b64bd414c8c52361b1fd6986
uhrwecker <janmenno@yahoo.de>
2018-09-11 22:07:12 +0200

    ntroduced Tags and TagHandler for later use

---
+++ infonary/tags/taghandler.py
@@ -0,0 +1,41 @@
+import tags as TT
+
+class TagHandler():
+    def __init__(self, tags):
+        self.tags = self.__initialize_tags(tags)
+
+    def add_tag(self, tag):
+        if tag in [tag_obj.get(call='ID') for tag_obj in self.tags]:
+            return self.tags
+        else:
+            self.tags.append(TT.__dict__[tag]())
+            return self.tags
+
+    def delete_tag(self, tag):
+        for tag_obj in self.tags:
+            if tag_obj.get(call='ID') == tag:
+                self.tags.remove(tag_obj)
+                return self.tags
+        return self.tags
+
+    def get(self):
+        return self.tags
+
+    def __initialize_tags(self, tags, **kwargs):
+        # Check if tags are doubled:
+        tags = list(set(tags))
+
+        taglist = [TT.__dict__[tag_id]() for tag_id in tags]
+        return taglist
+
+
+
+
+def main():
+    th = TagHandler(['Info', 'History', 'Info'])
+    th.add_tag('Info')
+    th.add_tag('Note')
+    th.delete_tag('History')
+
+if __name__ == '__main__':
+    main()
---
+++ infonary/tags/tags.py
@@ -0,0 +1,62 @@
+class Tag():
+    def __init__(self, tag_id, **kwargs):
+        self.verbose = False
+        self.information = {}
+        self.information['ID'] = tag_id
+        self.information.update(kwargs)
+
+    def get(self, call='all'):
+        if call == 'all':
+            return self.information
+        else:
+            return self.information[call]
+
+    def edit(self, edition, call='all'):
+        if call == 'all':
+            assert type(edition) == dict, 'If call-keyword is wanted, you have to pass a dictionary.'
+            self.information = edition
+            return self.information
+        else:
+            self.information[call] = edition
+            return self.information
+
+class Info(Tag):
+    def __init__(self, **kwargs):
+        self.id = 'Info'
+        super().__init__(self.id, **kwargs)
+
+class Idea(Tag):
+    def __init__(self, **kwargs):
+        self.id = 'Idea'
+        super().__init__(self.id, **kwargs)
+
+class History(Tag):
+    def __init__(self, **kwargs):
+        self.id = 'History'
+        super().__init__(self.id, **kwargs)
+
+class Note(Tag):
+    def __init__(self, **kwargs):
+        self.id = 'Note'
+        super().__init__(self.id, **kwargs)
+
+
+
+
+        
+
+def main():
+    info = Info()
+    idea = Idea()
+    hist = History(**{'date': '20.5.1634'})
+    note = Note()
+
+    print(info.get())
+    print(idea.get(call='ID'))
+    print(hist.get(call='date'))
+    print(note.edit('21.6.1984', call='EditDate'))
+
+if __name__ == '__main__':
+    main()
+    
+            
